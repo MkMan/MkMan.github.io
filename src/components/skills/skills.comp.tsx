e740ce78779f936b1137b3f4ed6ecc3d4e28c5a1
Mustafa Mansour <mustafa.k.mansour@gmail.com>
2019-10-05 16:05:20 +1000

    WIP: Add secondary skills

--- src/components/skills/skills.comp.tsx
+++ src/components/skills/skills.comp.tsx
@@ -5,27 +5,38 @@ import './skills.comp.scss';
 
 export class SkillsComponent extends React.Component {
 
-
-  render() {
+  public render() {
     return(
       <div className="skills-component">
         <PictureTextComponent text="Angular" logo="angular"/>
         <PictureTextComponent text="JavaScript" logo="js"/>
-        {this.getOtherSkills()}
+        <ul className="other-skills">
+          {this.getOtherSkills()}
+        </ul>
       </div>
     );
   }
 
-  getOtherSkills(): React.ReactNode {
+  private getOtherSkills(): React.ReactNode {
     const otherSkills = [
-      'Git'
+      'TypeScript',
+      'CSS',
+      'SCSS',
+      'Jasmine',
+      'Karma',
+      'Protractor',
+      'Git',
+      'Cypress',
+      'Webpack',
+      'Python',
+      'Java',
     ];
 
     const returnVal: React.ReactNode[] = [];
 
     otherSkills.forEach((skill) => {
       returnVal.push(
-        <p>{skill}</p>
+        <li>{skill}</li>,
       );
     });
 
